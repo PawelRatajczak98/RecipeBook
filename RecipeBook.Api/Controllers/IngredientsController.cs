72612c91211ae0134331dade1ce075f322b41287
PawelRatajczak <pawelratajczak1998@gmail.com>
2025-03-25 01:20:47 +0100

    TokenGetId

--- RecipeBook.Api/Controllers/IngredientsController.cs
+++ RecipeBook.Api/Controllers/IngredientsController.cs
@@ -1,10 +1,12 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
 using RecipeBook.Api.Entities;
 using RecipeBook.Api.Services;
 using System.Threading.Tasks;
 
 namespace RecipeBook.Api.Controllers
 {
+    [Authorize]
     [Route("api/[controller]")]
     [ApiController]
     public class IngredientController : ControllerBase
